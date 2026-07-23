03d0540cba9646232ebd2fe85ca48b95a10be846
slandath <jesus_slanda@yahoo.com>
2026-07-23 18:51:04 -0400

    deployment fixes
    Signed-off-by: slandath <jesus_slanda@yahoo.com>

--- server/server.ts
+++ server/server.ts
@@ -46,7 +46,7 @@ fastify.get("/", async () => ({ hello: "world" }));
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 });
+    await fastify.listen({ port: Number(process.env.PORT) || 3000, host: "0.0.0.0" });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
