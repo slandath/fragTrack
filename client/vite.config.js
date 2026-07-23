a3ba5baf345d71090c9bb161fb82a9c1384f5512
slandath <jesus_slanda@yahoo.com>
2026-07-22 21:54:08 -0400

    react installed
    Signed-off-by: slandath <jesus_slanda@yahoo.com>

---
+++ client/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    proxy: {
+      "/trpc": "http://localhost:3000",
+      "/api/auth": "http://localhost:3000",
+    },
+  },
+})
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "drizzle-kit";
 import "dotenv/config";
 
 export default defineConfig({
-    out: "./src/db/migrations",
-    schema: "./src/db/schema.ts",
+    out: "./server/db/migrations",
+    schema: "./server/db/schema.ts",
     dialect: "postgresql",
     dbCredentials: {
         url: process.env.DATABASE_URL!,
