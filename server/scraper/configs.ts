532dcfcbc784f0b80207943bcd72fa12c9b55cfc
slandath <jesus_slanda@yahoo.com>
2026-07-24 16:46:01 -0400

    added more sites
    Signed-off-by: slandath <jesus_slanda@yahoo.com>

--- server/scraper/configs.ts
+++ server/scraper/configs.ts
@@ -12,4 +12,29 @@ export const domainConfigs: Record<string, DomainConfig> = {
       currency: "USD",
     },
   },
+  "www.beautyhouse.com": {
+    selectors: {
+      price: ".h4 text-on-sale > span:first-child",
+      currency: "USD",
+    },
+  },
+  "www.fragflex.com": {
+    selectors: {
+      price:
+        "price-item price-custom-item price-item--sale price-item--last  price-custom-product-page   sale-price",
+      currency: "",
+    },
+  },
+  "www.perfumebox.com": {
+    selectors: {
+      price: "product__price on-sale",
+      currency: "USD",
+    },
+  },
+  "www.fragrancex.com": {
+    selectors: {
+      price: ".price-value",
+      currency: "USD",
+    },
+  },
 };
